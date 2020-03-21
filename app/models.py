b1e45ab0a1c13ede2f1d8b22bd08fc59180fe2f2
Tanvir3ds <tanayeem15@gmail.com>
2020-03-21 16:27:26 +0600

    slug fiels add

--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from django.db.models.signals import pre_save, post_save
+
+from project.utils import unique_slug_generator
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, null=True, blank=True)
+    #author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    text = models.TextField()
+    published_at = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ('-published_at',)
+
+    def __str__(self):
+        return self.title
+
+
+def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+
+pre_save.connect(rl_pre_save_receiver, sender=Post)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from app.models import Post
 
 # Create your views here.
+
+
+def home(request):
+    texts= Post.objects.all()
+    return render(request, "home.html",{'texts': texts})
+
+
+def detail(request, slug):
+
+    q = Post.objects.filter(slug__iexact=slug)
+    if q.exists():
+        q = q.first()
+    else:
+        return HttpResponse('<h1>Post Not Found</h1>')
+    context = {
+
+        'post': q
+    }
+    return render(request, 'details.html', context)
